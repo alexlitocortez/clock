42f1d79ad9e9f23c4f9b091c142957328f161da1
Alex Cortez <acim650@gmail.com>
2020-12-05 10:41:17 -0800

    clock.js

--- clock.js
+++ clock.js
@@ -1,12 +1,24 @@
 
 const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-    console.log(seconds);
+
+    const minutes = now.getMinutes();
+    const minsDegrees = ((minutes / 60) *360) + 90;
+    hourHand.style.transform = `rotate(${minsDegrees})deg`;
+
+    const hours = now.getMinutes();
+    const hourDegrees = (( mins / 12 ) * 360) + 90;
+    hourHand.style.transform = `rotate(${minsDegrees})deg`;
 }
 
 setInterval(setDate, 1000);
+
+
+
